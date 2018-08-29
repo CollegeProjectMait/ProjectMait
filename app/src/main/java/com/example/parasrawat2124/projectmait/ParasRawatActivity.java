a772c98b0084b47a0878c56c5cc6fd5ffa2131f6
Paras Rawat <rawatparas.kkdp@gmail.com>
2018-08-29 20:35:50 +0530

    ParasRawatActivity added

--- app/src/main/java/com/example/parasrawat2124/projectmait/ParasRawatActivity.java
+++ app/src/main/java/com/example/parasrawat2124/projectmait/ParasRawatActivity.java
@@ -3,15 +3,11 @@ package com.example.parasrawat2124.projectmait;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ParasRawatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int a,b;
-
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_paras_rawat);
     }
 }

6d2fb60c27a4ac54b3ea04d5f2f86aa143dd53cc
Paras Rawat <parasmyname@gmail.com>
2018-08-30 20:28:49 +0530

    Register Activity Added

--- app/src/main/java/com/example/parasrawat2124/projectmait/LoginActivity.java
+++ app/src/main/java/com/example/parasrawat2124/projectmait/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.parasrawat2124.projectmait;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputLayout username,password;
+    TextView registerhere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username=findViewById(R.id.Username);
+        password=findViewById(R.id.Password);
+        registerhere=findViewById(R.id.registerhere);
+
+        registerhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterUserActivity.class));
+            }
+        });
     }
 }

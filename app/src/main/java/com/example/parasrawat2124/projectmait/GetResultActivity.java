8959b9e593f0dd7fa898f664795ec02056c04e35
W0nderWoman <guptamayuri.acmn@gmail.com>
2018-08-30 12:26:59 +0530

    changes

--- app/src/main/java/com/example/parasrawat2124/projectmait/GetResultActivity.java
+++ app/src/main/java/com/example/parasrawat2124/projectmait/GetResultActivity.java
@@ -2,6 +2,7 @@ package com.example.parasrawat2124.projectmait;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GetResultActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class GetResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_result);
+
+        Button b1=(Button) findViewById(R.id.getclass);
+        Button b2=(Button) findViewById(R.id.getroom);
+        Button b3=(Button) findViewById(R.id.getteacher);
+        Button b4=(Button) findViewById(R.id.gettimetable);
+
+
     }
+
 }

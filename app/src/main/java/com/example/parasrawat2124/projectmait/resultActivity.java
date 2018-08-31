85b98edd02e59a96575af1222179bd6e62c35884
W0nderWoman <guptamayuri.acmn@gmail.com>
2018-09-01 02:47:56 +0530

    addTTActivity added resultActivity modified

--- app/src/main/java/com/example/parasrawat2124/projectmait/resultActivity.java
+++ app/src/main/java/com/example/parasrawat2124/projectmait/resultActivity.java
@@ -1,21 +1,30 @@
 package com.example.parasrawat2124.projectmait;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
 public class resultActivity extends AppCompatActivity {
 
     LinearLayout llreq,llres;
+    FirebaseDatabase fbd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        fbd=FirebaseDatabase.getInstance();
 
         Bundle bundle=getIntent().getExtras();
         String searchvalue=bundle.getString("searchvalue");
@@ -45,5 +54,18 @@ public class resultActivity extends AppCompatActivity {
                 llreq.addView(spin);
             }
         }
+    }
+
+    public void setDateTimeField(View view){
+        final Calendar newCalendar=Calendar.getInstance();
+        DatePickerDialog datePickerDialog=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                newCalendar.set(i,i1,i2,0,0);
+                //dateEditText.setText(dateFormatter.format(dateSelected.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+       // dateEditText.setText(dateFormatter.format(newCalendar.getTime()));
+        datePickerDialog.show();
     }
 }

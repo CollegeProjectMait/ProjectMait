18bca8afb1f6d480f6d153ccc943bad9f8c9f240
Paras Rawat <parasmyname@gmail.com>
2018-10-13 02:06:59 +0530

    Button Style

--- app/src/main/java/com/example/parasrawat2124/projectmait/OtpVerificationActivity.java
+++ app/src/main/java/com/example/parasrawat2124/projectmait/OtpVerificationActivity.java
@@ -1,13 +1,26 @@
 package com.example.parasrawat2124.projectmait;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OtpVerificationActivity extends AppCompatActivity {
+    TextView verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification);
+        verify=findViewById(R.id.verify);
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OtpVerificationActivity.this,pushDataActivity.class));
+
+            }
+        });
+
     }
 }
